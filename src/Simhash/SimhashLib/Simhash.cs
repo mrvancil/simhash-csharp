9408090c4d79d894ba2478584d0a9ac8126c336e
Tadd Vancil <taddvancil@hotmail.com>
2015-11-12 11:53:06 -0800

    adding masks and hashing functions

--- src/Simhash/SimhashLib/Simhash.cs
+++ src/Simhash/SimhashLib/Simhash.cs
@@ -8,11 +8,54 @@ namespace SimhashLib
 {
     public class Simhash
     {
+        const int f = 64;
+
         public long value { get; set; }
         public Simhash(List<string> features)
         {
+            build_by_features(features);
             value = 0;
         }
 
+        private void build_by_features(List<string> features)
+        {
+            /*
+                "features" might be a list of unweighted tokens(a weight of 1
+                will be assumed), a list of(token, weight) tuples or
+                a token -> weight dict.
+            */
+
+            int[] v = setupFingerprint();
+            long[] masks = setupMasks();
+
+            foreach(string f in features)
+            {
+                
+            }
+
+        }
+
+        private int[] setupFingerprint()
+        {
+            int[] v = new int[f];
+            for (int i = 0; i < v.Length; i++) v[i] = 0;
+            return v;
+        }
+
+        private long[] setupMasks()
+        {
+            long[] masks = new long[f];
+            for (int i = 0; i < masks.Length; i++)
+            {
+                masks[i] = (long)1 << i;
+            }
+            return masks;
+        }
+
+        private int hashfunc(string x)
+        {
+
+            return 0;
+        }
     }
 }
