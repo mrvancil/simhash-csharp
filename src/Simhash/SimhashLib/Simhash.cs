466b65cb11667e070afd0b267090488bd29998c2
Tadd Vancil <taddvancil@hotmail.com>
2015-11-12 11:01:39 -0800

    adding first failing test

---
+++ src/Simhash/SimhashLib/Simhash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimhashLib
+{
+    public class Simhash
+    {
+        public long value { get; set; }
+        public Simhash(List<string> features)
+        {
+            value = 0;
+        }
+
+    }
+}
---
+++ src/Simhash/SimhashTests/TestSimhash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SimhashLib;
+
+namespace SimhashTests
+{
+    [TestClass]
+    public class TestSimhash
+    {
+        [TestMethod]
+        public void test_value()
+        {
+            List<string> features = new List<string>() { "aaa", "bbb" };
+            var simHash = new Simhash(features);
+            Assert.AreEqual(simHash.value, 8637903533912358349);
+        }
+    }
+}
