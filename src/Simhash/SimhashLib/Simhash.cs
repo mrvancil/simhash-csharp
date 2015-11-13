f93b9c002bf8decfe68da3d587264dead83ec522
Tadd Vancil <taddvancil@hotmail.com>
2015-11-12 18:35:08 -0800

    adding first passing test for calculation of simhash

--- src/Simhash/SimhashLib/Simhash.cs
+++ src/Simhash/SimhashLib/Simhash.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
 
 namespace SimhashLib
 {
     public class Simhash
     {
-        const int f = 64;
+        const int fpSize = 64;
 
-        public long value { get; set; }
+        public ulong value { get; set; }
+        public Simhash()
+        {
+        }
+
         public Simhash(List<string> features)
         {
             build_by_features(features);
-            value = 0;
         }
 
         private void build_by_features(List<string> features)
@@ -26,36 +32,74 @@ namespace SimhashLib
             */
 
             int[] v = setupFingerprint();
-            long[] masks = setupMasks();
+            ulong[] masks = setupMasks();
 
-            foreach(string f in features)
+            foreach (string feature in features)
             {
-                
+                BigInteger h = hashfunc(feature);
+                int w = 1;
+                for (int i = 0; i < fpSize; i++)
+                {
+                    //convert to BigInt so we can use BitWise
+                    BigInteger bMask = masks[i];
+                    BigInteger result = h & bMask;
+                    v[i] += (result > 0) ? w : -w;
+                }
             }
 
-        }
+            ulong ans = 0;
+            //ulong uans = 0;
+            for (int i = 0; i<fpSize; i++)
+            {
+                if (v[i] >= 0)
+                {
+                    ans |= masks[i];
+                }
+            }
+            value = ans;
+         }
 
         private int[] setupFingerprint()
         {
-            int[] v = new int[f];
+            int[] v = new int[fpSize];
             for (int i = 0; i < v.Length; i++) v[i] = 0;
             return v;
         }
 
-        private long[] setupMasks()
+        private ulong[] setupMasks()
         {
-            long[] masks = new long[f];
+            ulong[] masks = new ulong[fpSize];
             for (int i = 0; i < masks.Length; i++)
             {
-                masks[i] = (long)1 << i;
+                masks[i] = (ulong)1 << i;
             }
             return masks;
         }
 
-        private int hashfunc(string x)
+        private BigInteger hashfunc(string x)
         {
+            string hexValue = hashfunc_hashtostring(x);
+            BigInteger b = hashfunc_hashstringtobignasty(hexValue);
+            return b;
+        }
+        public string hashfunc_hashtostring(string x)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(x));
 
-            return 0;
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+        public BigInteger hashfunc_hashstringtobignasty(string x)
+        {
+            BigInteger bigNumber = BigInteger.Parse(x, NumberStyles.AllowHexSpecifier);
+            return bigNumber;
         }
     }
 }
--- src/Simhash/SimhashTests/TestSimhash.cs
+++ src/Simhash/SimhashTests/TestSimhash.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using SimhashLib;
+using System.Numerics;
 
 namespace SimhashTests
 {
@@ -9,11 +10,29 @@ namespace SimhashTests
     public class TestSimhash
     {
         [TestMethod]
+        public void test_hashtostringvalue()
+        {
+            var simHash = new Simhash();
+            string val = simHash.hashfunc_hashtostring("aaa");
+            Assert.AreEqual(val, "47bce5c74f589f4867dbd57e9ca9f808");
+        }
+        [TestMethod]
+        public void test_hashstringtobigint()
+        {
+            var simHash = new Simhash();
+            BigInteger actualBiggie = simHash.hashfunc_hashstringtobignasty("47bce5c74f589f4867dbd57e9ca9f808");
+            string expectedBiggie = "95355999972893604581396806948474189832";
+            Assert.AreEqual(expectedBiggie, actualBiggie.ToString());
+        }
+
+        [TestMethod]
         public void test_value()
         {
             List<string> features = new List<string>() { "aaa", "bbb" };
             var simHash = new Simhash(features);
-            Assert.AreEqual(simHash.value, 8637903533912358349);
+            ulong expected = 8637903533912358349;
+            Assert.AreEqual(expected,simHash.value );
         }
+
     }
 }
