c8c0b7320d33fc9961b9f5733f2b4d1c3808255d
Tadd Vancil <taddvancil@hotmail.com>
2015-11-12 20:44:11 -0800

    refactor. moving some converters out of test to Simhash!

--- src/Simhash/SimhashLib/Simhash.cs
+++ src/Simhash/SimhashLib/Simhash.cs
@@ -105,5 +105,17 @@ namespace SimhashLib
             BigInteger bigNumber = BigInteger.Parse(x, NumberStyles.AllowHexSpecifier);
             return bigNumber;
         }
+
+        public static string convert_ulong_to_bin(ulong value)
+        {
+            if (value == 0) return "0";
+            System.Text.StringBuilder b = new System.Text.StringBuilder();
+            while (value != 0)
+            {
+                b.Insert(0, ((value & 1) == 1) ? '1' : '0');
+                value >>= 1;
+            }
+            return b.ToString();
+        }
     }
 }
--- src/Simhash/SimhashTests/TestSimhash.cs
+++ src/Simhash/SimhashTests/TestSimhash.cs
@@ -9,29 +9,19 @@ namespace SimhashTests
     [TestClass]
     public class TestSimhash
     {
-        public static string ConvertToBinary(ulong value)
-        {
-            if (value == 0) return "0";
-            System.Text.StringBuilder b = new System.Text.StringBuilder();
-            while (value != 0)
-            {
-                b.Insert(0, ((value & 1) == 1) ? '1' : '0');
-                value >>= 1;
-            }
-            return b.ToString();
-        }
+        
 
         [TestMethod]
         public void test_ulongtolongbacktoulong()
         {
             ulong theUlong = 18446744073709551615;
-            string stheUlong = ConvertToBinary(theUlong);
+            string stheUlong = Simhash.convert_ulong_to_bin(theUlong);
             long cLong = Convert.ToInt64(stheUlong, 2);
             //save to mongo or other db using long (as ulong aren't!)
             //retrieve from db and then get back to ulong
             string sLong = Convert.ToString(cLong, 2);
             ulong fromDb = Convert.ToUInt64(sLong,2);
-            string thedbUlong = ConvertToBinary(fromDb);
+            string thedbUlong = Simhash.convert_ulong_to_bin(fromDb);
             
             Assert.AreEqual(stheUlong, thedbUlong);
         }
@@ -39,7 +29,7 @@ namespace SimhashTests
         {
             //ulong theUlong = 8637903533912358349;
             ulong theUlong = 18446744073709551615;
-            string stheUlong = ConvertToBinary(theUlong);
+            string stheUlong = Simhash.convert_ulong_to_bin(theUlong);
             string expSimHash = "111011111011111111111110111111110011110111011111111110111001101";
             Assert.AreEqual(expSimHash, stheUlong);
         }
