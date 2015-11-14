af22c763e9fba34ef0873032281b561a9d765d6a
Tadd Vancil <taddvancil@hotmail.com>
2015-11-13 20:43:16 -0800

    implemented offset logic , based off k param

--- src/Simhash/SimhashLib/SimhashIndex.cs
+++ src/Simhash/SimhashLib/SimhashIndex.cs
@@ -8,9 +8,68 @@ namespace SimhashLib
 {
     public class SimhashIndex
     {
-        public SimhashIndex(Dictionary<long,Simhash> objs,int k)
+        public int kDistance;
+        public int fpSize = 64;
+        public Dictionary<string, string> bucket;
+        public List<int> offsets;
+
+        public SimhashIndex(Dictionary<long,Simhash> objs, int f = 64, int k=2 )
         {
+            this.kDistance = k;
+            this.fpSize = f;
+            //Console.WriteLine("Initializing {0} data.", objs.Count);
+            bucket = new Dictionary<string, string>();
+
+            make_offsets();
+
+            foreach(KeyValuePair<long, Simhash> q in objs)
+            {
+                add(q.Key, q.Value);
+            }
 
         }
+        private void add(long obj_id, Simhash simhash)
+        {
+            //foreach(string key in get_keys(simhash))
+            //{ }
+
+
+        }
+        public List<int> make_offsets()
+        {
+            var ans = new List<int>();
+            for (int i=0;i<(kDistance+1);i++)
+            {
+                int offset = fpSize / (kDistance + 1) * i;
+                ans.Add(offset);
+            }
+            return ans;
+        }
+
+        //public static IEnumerable<string> get_keys(Simhash simhash)
+        //{
+
+        //    for (int i = 0; i < exponent; i++)
+        //    {
+        //        result = result * number;
+        //        yield return result;
+        //    }
+        //}
+
+
+        public HashSet<long> get_near_dups(Simhash simhash)
+        {
+            /*
+            "simhash" is an instance of Simhash
+            return a list of obj_id, which is in type of long (for now)
+            */
+            if (simhash.fpSize != this.fpSize) throw new Exception();
+
+
+            var ans = new HashSet<long>();
+            return ans;
+        }
+
+
     }
 }
--- src/Simhash/SimhashTests/TestSimhashIndex.cs
+++ src/Simhash/SimhashTests/TestSimhashIndex.cs
@@ -23,14 +23,38 @@ namespace SimhashTests
             foreach(var it in data)
             {
                 objs.Add(it.Key, new Simhash(it.Value));
-                index = new SimhashIndex(objs, 10);
+                index = new SimhashIndex(objs:objs,k:10);
             }
 
         }
         [TestMethod]
+        public void test_offset_creation_with_ten()
+        {
+            var dict = new Dictionary<long, Simhash>();
+            var simHashIndex = new SimhashIndex(dict, k: 10);
+            var offsets = simHashIndex.make_offsets();
+            Assert.AreEqual(0, offsets[0]);
+            Assert.AreEqual(10, offsets[2]);
+            Assert.IsTrue(offsets.Count == 11);
+        }
+        [TestMethod]
+        public void test_offset_creation_with_two()
+        {
+            var dict = new Dictionary<long, Simhash>();
+            var simHashIndex = new SimhashIndex(dict, k: 2);
+            var offsets = simHashIndex.make_offsets();
+            Assert.AreEqual(0, offsets[0]);
+            Assert.AreEqual(42, offsets[2]);
+            Assert.IsTrue(offsets.Count == 3);
+        }
+
+        [Ignore]
+        [TestMethod]
         public void test_get_near_dup()
         {
-
+            var s1 = new Simhash("How are you i am fine.ablar ablar xyz blar blar blar blar blar blar blar thank");
+            var dups = index.get_near_dups(s1);
+            Assert.AreEqual(dups.Count, 3);
         }
     }
 }
